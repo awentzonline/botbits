b07b17560a13941e015088525ec9fcac14dc703a
Adam Wentz <awentz@theonion.com>
2014-03-15 12:40:27 -0600

    Added setup.py OPwNd

---
+++ setup.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import re
+import sys
+
+from setuptools import setup
+
+
+name = "botbits"
+package = "botbits"
+description = "Bits for building bots."
+url = "https://github.com/awentzonline/botbits"
+author = "Adam Wentz, Chris Sinchok"
+author_email = "awentz@theonion.com"
+license = "MIT"
+requires = [
+    "greenlet==0.4.2",
+    "gevent==1.0",
+    "tweepy",
+    "pytumblr",
+    "geventirc"
+],
+dependency_links = [
+    "https://github.com/awentzonline/geventirc/tarball/master#egg=geventirc",
+]
+
+
+def get_version(package):
+    """
+    Return package version as listed in `__version__` in `init.py`.
+    """
+    init_py = open(os.path.join(package, "__init__.py")).read()
+    return re.search("^__version__ = ['\"]([^'\"]+)['\"]", init_py, re.MULTILINE).group(1)
+
+
+def get_packages(package):
+    """
+    Return root package and all sub-packages.
+    """
+    return [dirpath
+            for dirpath, dirnames, filenames in os.walk(package)
+            if os.path.exists(os.path.join(dirpath, "__init__.py"))]
+
+
+def get_package_data(package):
+    """
+    Return all files under the root package, that are not in a
+    package themselves.
+    """
+    walk = [(dirpath.replace(package + os.sep, "", 1), filenames)
+            for dirpath, dirnames, filenames in os.walk(package)
+            if not os.path.exists(os.path.join(dirpath, "__init__.py"))]
+
+    filepaths = []
+    for base, filenames in walk:
+        filepaths.extend([os.path.join(base, filename)
+                          for filename in filenames])
+    return {package: filepaths}
+
+
+if sys.argv[-1] == "publish":
+    os.system("python setup.py sdist upload")
+    args = {"version": get_version(package)}
+    print "You probably want to also tag the version now:"
+    print "  git tag -a %(version)s -m 'version %(version)s'" % args
+    print "  git push --tags"
+    sys.exit()
+
+
+setup(
+    name=name,
+    version=get_version(package),
+    url=url,
+    license=license,
+    description=description,
+    author=author,
+    author_email=author_email,
+    packages=get_packages("botbits"),
+    package_data=get_package_data(package),
+    install_requires=requires,
+    dependency_links=dependency_links
+)
