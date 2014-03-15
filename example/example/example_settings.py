b07b17553d5d46bdb29fb74decbe0cfef51df3a6
Adam Wentz <awentz@theonion.com>
2014-03-15 13:05:30 -0600

    Added example bot iTEZx

---
+++ example/example/example_settings.py
@@ -0,0 +1,11 @@
+NICK = "coolnick"
+HOST = "irc.something.something"
+CHANNELS = (u"#cool",)
+PASSWORD = "hunter2"
+TWITTER = {
+    # "CONSUMER_KEY": "coool",
+    # "CONSUMER_SECRET": "creds",
+    # "TOKEN_KEY": "guy",
+    # "TOKEN_SECRET": "!"
+}
+BAD_WORDS = ("yolo",)
