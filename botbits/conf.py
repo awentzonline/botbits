4fac04f53103848a3ef1a3de2076dc26c7eab5f5
Adam Wentz <awentz@theonion.com>
2014-03-21 16:55:10 -0500

    Added fancy yaml loader

--- botbits/conf.py
+++ botbits/conf.py
@@ -5,6 +5,11 @@ import yaml
 BOTBITS_CONF = "BOTBITS_CONF"
 
 
+class NonprintableLoader(yaml.Loader):
+    def check_printable(self, data):
+        return True
+
+
 class LazySettings(object):
 
     def __init__(self, filename=None):
@@ -13,7 +18,7 @@ class LazySettings(object):
     def __getitem__(self, name):
         if self._config is None:
             with open(os.environ.get(BOTBITS_CONF, "botconf.yml"), "rb") as stream:
-                self._config = yaml.load(stream)
+                self._config = yaml.load(stream, NonprintableLoader)
         return self._config.__getitem__(name)
 
     def get(self, name, default=None):
